487a098232f35c293212a4246db25842376c0323
RuslanSalimov <mail-spam2012@yandex.ru>
2021-02-26 15:40:18 +0500

    сладер на сайте готов

--- js/script.js
+++ js/script.js
@@ -36,7 +36,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function addZero(number) {
         return +number < 10 ? ('0' + number) : number;
     }
-
+    /**
+     * Функция плавного скролла до элемента, чистая, работает как вверх, так и вниз (писал сам)))
+     * @param element - ссылка на элемент
+     * @param duration - продолжительность скролла в мс
+     */
     const scrollToElement = (element, duration) => {
         let Id; //id анимации
         let start = performance.now();  //время старта
@@ -156,14 +160,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const addScrollToElements = () => {
         const serviceBlock = document.querySelector('.service');//блок услуги
         const serviceBlockAnchor = document.querySelector('a[href="#service-block"]');//ссылка на блок услуги
-        const menu = document.querySelector('menu');//меню
-        const menuItems = menu.querySelectorAll('ul>li');//пункты меню
-        /**
-         * Функция плавного скролла до элемента, чистая, работает как вверх, так и вниз (писал сам)))
-         * @param element - ссылка на элемент
-         * @param duration - продолжительность скролла в мс
-         */
-
         //добавляем скроллы на ссылку
         serviceBlockAnchor.addEventListener('click', (e) => {
             e.preventDefault();
@@ -195,5 +191,111 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     serviceTabs();
 
+    /**
+     * Запуск и инициализация слайдера
+     * @param interval
+     */
+    const slider = (interval) => {
+        const slider = document.querySelector('.portfolio-content');//сам слайдер
+        const slides = document.querySelectorAll('.portfolio-item');//слайды
+        const dotsContainer = document.querySelector('.portfolio-dots');//контейнер для точек
+        let dots = [];              //массив с будущими точками
+        let currentSlide = 0;       //текущий слайд
+        let intervalID;     //id для setInterval
+        /**
+         * Добавление точек
+         */
+        const addDots = () => {
+            for(let i = 0; i < slides.length; i++){
+                let dot = document.createElement('li');
+                dot.classList.add('dot');
+                if(i === 0){
+                    dot.classList.add('dot-active');
+                }
+                dots.push(dot);
+                dotsContainer.append(dot);
+            }
+
+        };
+        addDots();
+        /**
+         * проверка номера текущего слайда, если что не так, корректирует
+         */
+        const checkSlideNumber = () => {
+          if(currentSlide < 0){
+              currentSlide = slides.length - 1;
+          } else if(currentSlide >= slides.length){
+              currentSlide = 0;
+          }
+        };
+        /**
+         * Переход на следующий слайд
+         * @param next - если true, то следующий, false - предыдущий
+         */
+        const clickNextSlide = (next = true) =>{
+            slides[currentSlide].classList.remove('portfolio-item-active');
+            dots[currentSlide].classList.remove('dot-active');
+            if (next){
+                currentSlide++;
+            } else {
+                currentSlide--;
+            }
+            checkSlideNumber();
+            slides[currentSlide].classList.add('portfolio-item-active');
+            dots[currentSlide].classList.add('dot-active');
+        };
+        /**
+         * Запуск слайдера в автоплей
+         */
+        const playSlider = () => {
+            intervalID = setInterval(clickNextSlide, interval, true);
+        };
+        /**
+         * Остановка слайдера
+         */
+        const stopSlider = () => {
+            clearInterval(intervalID);
+        };
+        playSlider();
+        /**
+         * клики через делегирование
+         */
+        slider.addEventListener('click', (e) => {
+            e.preventDefault();
+            let target = e.target;
+            if(!target.matches('.portfolio-btn, .dot')){
+                return;
+            }
+            //клики по кнопкам
+            if(target.matches('#arrow-right')){
+                clickNextSlide(true);
+            } else if(target.matches('#arrow-left')){
+                clickNextSlide(false);
+            } else if(target.matches('.dot')){          //клики по точкам
+                slides[currentSlide].classList.remove('portfolio-item-active');
+                dots[currentSlide].classList.remove('dot-active');
+                currentSlide = dots.indexOf(target);
+                slides[currentSlide].classList.add('portfolio-item-active');
+                dots[currentSlide].classList.add('dot-active');
+            }
+        });
+        //остановка слайдера при наведении на точки и стрелки
+        slider.addEventListener('mouseover', (e) => {
+            let target = e.target;
+            if(target.matches('.portfolio-btn') || target.matches('.dot')){
+                stopSlider();
+            }
+        });
+        //запуск слайдера после того, как мышку убрали с точек и стрелок
+        slider.addEventListener('mouseout', (e) => {
+            let target = e.target;
+            if(target.matches('.portfolio-btn') || target.matches('.dot')){
+                playSlider();
+            }
+        });
+
+    };
+
+    slider(3000);
 
 });
