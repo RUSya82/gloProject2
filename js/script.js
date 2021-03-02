79b6d5ec8cffab155b80d54a16f62eb66b83e2a6
RuslanSalimov <mail-spam2012@yandex.ru>
2021-03-03 01:11:44 +0500

    Выполнено задание основное

--- js/script.js
+++ js/script.js
@@ -389,8 +389,41 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     validFeedbackForm();
 
-    const calc = () => {
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcCount = document.querySelector('.calc-count');
+        const calcDay = document.querySelector('.calc-day');
+        const totalBlock = document.getElementById('total');
 
+        const calcSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1)/10;
+            }
+            if(calcDay.value && calcDay.value < 5){
+                dayValue = 2;
+            } else if (calcDay.value && calcDay.value < 10){
+                dayValue = 1.5;
+            }
+            if(typeValue && squareValue){
+                total  =  price * countValue * dayValue * typeValue * squareValue;
+            }
+            totalBlock.innerHTML = Math.floor(total);
+        };
+
+        calcBlock.addEventListener('change', (e) => {
+            let target = e.target;
+            if(target.matches('input, select')){
+                calcSum();
+            }
+        });
     };
-
+    calc(100);
 });
